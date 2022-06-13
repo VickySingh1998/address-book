e0c8d45f4b8ae305ef99fbc5e77101383bf276e0
VickySingh1998 <vickysingh8697@gmail.com>
2022-06-14 00:10:43 +0530

    added: new class and created contact list

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -1,10 +1,34 @@
 package com.bridgelabz;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class AddressBookSystem {
-    public static void addressBook(){
-        System.out.println("Welcome to Address Book Program");
-    }
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        addressBook();
+        System.out.println("Welcome to address book");
+        Contact contact = addContact();
+        System.out.println(contact);
+    }
+
+    private static Contact addContact() {
+        Contact contact = new Contact();
+        System.out.println("Enter First name:");
+        contact.setFirstName(scanner.next());
+        System.out.println("Enter Last name:");
+        contact.setLastName(scanner.next());
+        System.out.println("Enter Address:");
+        contact.setAddress(scanner.next());
+        System.out.println("Enter City :");
+        contact.setCity(scanner.next());
+        System.out.println("Enter State :");
+        contact.setState(scanner.next());
+        System.out.println("Enter Zip Code :");
+        contact.setZip(scanner.next());
+        System.out.println("Enter Phone Number :");
+        contact.setPhoneNumber(scanner.next());
+        System.out.println("Enter Email :");
+        contact.setEmail(scanner.next());
+        return contact;
     }
 }
